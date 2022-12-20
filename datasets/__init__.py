19d0608a85ee9d1a1d32a08f28a52922edbfbc72
Alexandre Englebert <alexandre.englebertdenis@gmail.com>
2022-12-20 20:06:11 +0100

    Datasets: add albumentations support for imagenet

---
+++ datasets/__init__.py
@@ -0,0 +1,1 @@
+from .ilsvrc2012 import AlbumentationsImageNet
---
+++ datasets/ilsvrc2012.py
@@ -0,0 +1,20 @@
+from torchvision.datasets import ImageNet
+
+import numpy as np
+
+from albumentations.core.transforms_interface import BasicTransform
+from albumentations.core.composition import BaseCompose
+
+
+class AlbumentationsImageNet(ImageNet):
+    def __init__(self, transform=None, **kwargs):
+
+        if isinstance(transform, BasicTransform) or isinstance(transform, BaseCompose):
+            def transform_fn(image):
+                np_image = np.array(image)
+                return transform(image=np_image)['image']
+
+        else:
+            transform_fn = transform
+
+        super().__init__(transform=transform_fn, **kwargs)
