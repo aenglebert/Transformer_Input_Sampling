01f9e665421417a38387ad7b97b33f7e1c429e25
Alexandre Englebert <alexandre.englebertdenis@gmail.com>
2023-01-13 10:23:12 +0100

    sensitivity max: more comments

--- metrics/sensitivity.py
+++ metrics/sensitivity.py
@@ -6,8 +6,8 @@ from torchvision.transforms import Resize
 def captum_method_wrapper(method):
     """
     A wrapper for the explainibility method to return saliency in the format expected by captum
-    :param method:
-    :return:
+    :param method: The Captum xai method to wrap
+    :return: wrapped Captum method
     """
     def fn(inputs, target=None):
         # Set resize transformation for the saliency maps if upsampling is required
@@ -26,13 +26,29 @@ def captum_method_wrapper(method):
 
 
 class SensitivityMax:
+    """
+    Sensitivity Max metric class, use Captum function under the hood
+    """
     def __init__(self, model, method, n_perturb_samples=10, perturb_radius=0.02):
+        """
+        Init
+        :param model: model to explain
+        :param method: xai method to generate saliency maps to evaluate
+        :param n_perturb_samples: number of perturbations samples for sensitivity max metric
+        :param perturb_radius: radius of the perturbation for sensitivity max
+        """
         self.model = model
         self.attribute_fn = captum_method_wrapper(method)
         self.n_perturb_samples = n_perturb_samples
         self.perturb_radius = perturb_radius
 
     def __call__(self, image, target):
+        """
+        call the metric
+        :param image: input to use
+        :param target: target class for the xai method
+        :return:
+        """
         sens = sensitivity_max(self.attribute_fn,
                                image,
                                target=target,
