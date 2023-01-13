c4e81ddfc8b89128ac8337887485ea6e5bdb7e4e
Sedr1ck <yandrosdk@hotmail.com>
2023-01-13 12:56:02 +0100

    Sparseness metric update.

--- metrics/sparseness.py
+++ metrics/sparseness.py
@@ -33,19 +33,16 @@ class Sparseness:
         
         # Avoid 0 values in the saliency map
         flat_saliency = torch.add(flat_saliency, 0.0000001)
-        print("flat:", flat_saliency)
         # Length of the flattened tensor
         length = flat_saliency.size()[0]
-        print("length:", length)
         
         
         # Sort the tensor in ascending order
         sorted_saliency = torch.sort(flat_saliency)
-        print("sorted:", sorted_saliency)
         
         # Rank of each item of the sorted tensor: from 1 to length+1
         sorted_rank = torch.arange(start=1, end=length+1, step=1)
-        print("rank:", sorted_rank)
+
         # Compute Sparseness 
         # (possible values :[0,1] with 0 perfect equality and 1 perfect inequality for an infini)
         # based on the simplification of the equation
