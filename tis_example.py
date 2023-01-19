4331f179f825fceccb5aab4ce5239562b17dc3f0
Alexandre Englebert <alexandre.englebertdenis@gmail.com>
2023-01-19 12:39:25 +0100

    Example: use hydra to generate on custom image

--- tis_example.py
+++ tis_example.py
@@ -1,27 +1,76 @@
-from torchvision import transforms
+import hydra
+from hydra.utils import instantiate
+from omegaconf import DictConfig, OmegaConf
 
-# Load a ViT model
-import timm
-model = timm.create_model("vit_base_patch16_224", pretrained=True).cuda()
-model.eval()
+import torch
+from torch.nn.functional import interpolate
+import numpy as np
+import random
 
-# Set tranforms, normalise to ImageNet train mean and sd 
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Resize((256, 256)),
-                                transforms.CenterCrop(224),
-                                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-                                ])
+from datasets.ilsvrc2012 import classes
 
-# Get image 
-from PIL import Image 
-image = Image.open("dog.png").convert('RGB') 
-input_tensor = transform(image).cuda()
+from PIL import Image
 
-# Initialize the saliency class (adapt the batch_size depending on the available memory)
-from tis import TIS
-saliency_method = TIS(model, batch_size=512)
+from matplotlib import pyplot as plt
 
-# class_idx can be omited, in this case the maximum predicted class will be used
-saliency_map = saliency_method(input_tensor, 
-                   #class_idx=class_idx
-                  ).cpu()
+
+# Define a function to seed everything
+def seed_everything(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+def overlay(image, saliency, alpha=0.7):
+    fig, ax = plt.subplots(1, 2, figsize=(10, 6))
+    image = image.permute(1, 2, 0)
+    saliency = interpolate(saliency.reshape((1, 1, *saliency.shape)), size=image.shape[:2], mode='bilinear')
+    saliency = saliency.squeeze()
+    ax[0].imshow(image)
+    ax[1].imshow(image)
+    ax[1].imshow(saliency, alpha=alpha, cmap='jet')
+    plt.show()
+
+
+@hydra.main(version_base="1.3", config_path="config", config_name="example")
+def main(cfg: DictConfig):
+
+    seed_everything(cfg.seed)
+
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+    # Get model
+    print("Loading model:", cfg.model.name, end="\n\n")
+    model = instantiate(cfg.model.init).to(device)
+    model.eval()
+
+    # Get method
+    print("Initializing saliency method:", cfg.method.name, end="\n\n")
+    method = instantiate(cfg.method.init, model)
+
+    # Get transformations
+    print("Setting transformations", end="\n\n")
+    transform = instantiate(cfg.transform)
+
+    # Get image
+    print("Opening image:", cfg.input_file, end="\n\n")
+    image_raw = Image.open(cfg.input_file).convert('RGB')
+    image = transform(image_raw).to(device)
+
+    if not cfg.class_idx:
+        class_idx = torch.argmax(model(image.unsqueeze(0)), dim=-1)[-1]
+    else:
+        class_idx = cfg.class_idx
+
+    # Computing saliency map
+    print("Computing saliency map using", cfg.method.name, "for class", classes[class_idx])
+    saliency_map = method(image, class_idx=class_idx).detach().cpu()
+
+    image = image - image.min()
+    image = image/image.max()
+    overlay(image.cpu(), saliency_map)
+
+
+if __name__ == "__main__":
+    main()
